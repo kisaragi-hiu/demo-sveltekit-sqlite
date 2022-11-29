c3378c480e82e4b4d3c01f5d542afc59084df5d1
Kisaragi Hiu <mail@kisaragi-hiu.com>
2022-11-30 05:57:40 +0900

    Implement a searchbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,16 @@
 <script>
+  import SuperHeroes from "$lib/components/SuperHeroes.svelte";
   const description = "Here's my description!";
+  async function update(value) {
+    if (value.trim().length > 0) {
+      items = await fetch(`/heroes?q=${value.trim()}`).then((v) => v.json());
+    } else {
+      items = [];
+    }
+  }
+  let items = [];
+  let value;
+  $: update(value);
 </script>
 
 <svelte:head>
@@ -8,6 +19,16 @@
 </svelte:head>
 
 <main class="w-[95%] max-w-2xl mx-auto mt-48">
-<h1 class="text-3xl font-bold">Example reading data from a local SQLite database</h1>
-<p>Hello World!</p>
+  <h1 class="text-3xl font-bold">
+    Example reading data from a local SQLite database
+  </h1>
+  <p>Search for superheroes below</p>
+  <input
+    class="p-2"
+    type="search"
+    autocomplete="off"
+    placeholder="Search"
+    bind:value
+  />
+  <SuperHeroes {items} />
 </main>

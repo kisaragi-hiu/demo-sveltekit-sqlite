005387b55803019b98a08dadb5a95a3c24b7c7da
Kisaragi Hiu <mail@kisaragi-hiu.com>
2022-11-30 03:44:01 +0900

    Add the root page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  const description = "Here's my description!";
+</script>
+
+<svelte:head>
+  <meta name="description" content={description} />
+  <title>Here's a title!</title>
+</svelte:head>
+
+<h1>Example reading data from a local SQLite database</h1>
+<p>Hello World!</p>

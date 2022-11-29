3d5f5a22c65e7ee425a82a0c900645493ed94687
Kisaragi Hiu <mail@kisaragi-hiu.com>
2022-11-30 05:41:59 +0900

    Create the hero list component

---
+++ src/lib/components/SuperHeroes.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let items = [];
+</script>
+
+<ul>
+  {#each items as item}
+    <li class="w-full flex justify-between">
+      {item.name}{#if item.first_appearance_year}<span
+          >First appearance: {item.first_appearance_year}</span
+        >{/if}
+    </li>
+  {/each}
+</ul>
